310719660a229b930c2515962dec912773252a3e
Leandro <leandroquinteronavarro@gmail.com>
2020-10-19 06:46:42 -0500

    Primer parcial 191463

---
+++ src/app/components/pages/client/modalidades/modalidades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalidades',
+  templateUrl: './modalidades.component.html',
+  styleUrls: ['./modalidades.component.sass']
+})
+export class ModalidadesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
